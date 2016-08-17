2a8a492f5d78db7cfa24ba6219cebef6b9b789be
harmanssingh <harmanshahsingh@hotmail.com>
2016-08-17 15:35:25 -0700

    Homework, Day 10 without Seventeen

--- numbered_lines.py
+++ numbered_lines.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
-
+def numbered_lines():
+    with open('small.txt', 'r') as f:
+        txt=f.readlines()
+    for lines in txt:
+        lines.strip()
+    with open('smallnew.txt', 'w') as f1:
+        line=1
+        for lines in txt:
+            f1.write(str(line)+' '+lines)
+            line+=1
 
 def main():
     numbered_lines()
